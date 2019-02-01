59dc53aaa92a17528676f5fd82366622a84851e6
Haya <Haya@DESKTOP-A6J1G1T.mynet>
2019-02-01 11:13:44 -0800

    welcome

---
+++ src/haya/haya.java
@@ -0,0 +1,21 @@
+package haya;
+public class haya {
+	  public static void main(String[] args) 
+	    { 
+	       hw x;
+	       x=new hw();
+	       System.out.println("x="+x);
+	    } 
+	}
+class hw {
+	private int h ;
+	public hw()
+	{
+		h=10;
+		}
+  @Override
+  public String toString(){
+  	String p = "Class hw:H=" + h;
+  	return p;
+  }
+}
